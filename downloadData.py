99fd1274775390251f8e9e07cd980218948a1549
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-07 22:26:07 +0200

    complete rework

--- downloadData.py
+++ downloadData.py
@@ -1,17 +1,18 @@
-#!/usr/bin/env python3
 import argparse
 import wget
 
 parser = argparse.ArgumentParser(description='Download data and output it with pod id.')
 parser.add_argument('--url', type=str, default=100,
   help='Url to the file inside of cluster.')
+parser.add_argument('--data')
+parser.add_argument('--pod_name', type=str, help='GCS path of the output directory.')
+
 args = parser.parse_args()
 
 file_url = str(args.url)
 data = wget.download(file_url,'/data.csv')
-
 pod_name = open("/etc/hostname").read()
 print(str(pod_name))
-f = open("/pod.txt", 'w')
+f = open("/output.txt", 'w')
 f.write(str(pod_name))
 f.close()
