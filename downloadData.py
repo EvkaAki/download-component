111147e617400b03ced5b6f8bd61e4d1a358f352
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-07 22:51:02 +0200

    add output parameter

--- downloadData.py
+++ downloadData.py
@@ -4,13 +4,21 @@ import wget
 parser = argparse.ArgumentParser(description='Download data and output it with pod id.')
 parser.add_argument('--url', type=str, default=100,
   help='Url to the file inside of cluster.')
+parser.add_argument("--pod-path", type=str, help="Path to store URI output")
 
 args = parser.parse_args()
+podPath = args.pod_path
 
 file_url = str(args.url)
 data = wget.download(file_url,'/data.csv')
-pod_name = open("/etc/hostname").read()
-print(str(pod_name))
+pod = open("/etc/hostname").read()
+print(str(pod))
+
 f = open("/output.txt", 'w')
-f.write(str(pod_name))
+f.write(str(pod))
+f.close()
+f = open()
+
+f = open(podPath, 'w')
+f.write(str(pod))
 f.close()
